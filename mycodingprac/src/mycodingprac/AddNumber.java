cf4188689f4730a02890228e14bedb12637d2d94
satyajeet kumars singh <satyajeetsingh011@hgmail.com>
2023-01-05 20:11:52 +0530

    add number

---
+++ mycodingprac/src/mycodingprac/AddNumber.java
@@ -0,0 +1,18 @@
+package mycodingprac;
+import java.util.*;
+public class AddNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first number: ");
+		int a = sc.nextInt();
+		System.out.println("Enter the second number: ");
+		int b = sc.nextInt();
+		int c = a+b;
+		System.out.println("the sum of a and  b is:"+c);
+		
+		
+
+	}
+
+}
