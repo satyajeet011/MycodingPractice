5c225078f76a8c71b2f96299ee6ac3660d7729dc
satyajeet kumars singh <satyajeetsingh011@hgmail.com>
2023-01-05 20:00:20 +0530

    hello world added

---
+++ mycodingprac/src/mycodingprac/HelloWorld.java
@@ -0,0 +1,11 @@
+package mycodingprac;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
