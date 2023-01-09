758dd4a10c6bd3077a83d0fc0d5f8d5b45a4a281
satyajeet kumars singh <satyajeetsingh011@hgmail.com>
2023-01-09 23:25:34 +0530

    area

---
+++ mycodingprac/src/mycodingprac/AreaOfCircle.java
@@ -0,0 +1,14 @@
+package mycodingprac;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		double pi, r, a;
+		r = 10.8; 
+		pi = 3.1416; 
+		a = pi * r * r; 
+		System.out.println("Area of circle is " + a);
+
+	}
+
+}
