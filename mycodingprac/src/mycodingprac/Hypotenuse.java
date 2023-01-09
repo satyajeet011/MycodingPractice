d492c0c7dc580076587d7a4bb9ad2d707fca4fd5
satyajeet kumars singh <satyajeetsingh011@hgmail.com>
2023-01-09 23:18:28 +0530

    hypotenuse

---
+++ mycodingprac/src/mycodingprac/Hypotenuse.java
@@ -0,0 +1,12 @@
+package mycodingprac;
+
+public class Hypotenuse {
+
+	public static void main(String[] args) {
+		double a = 3.0, b = 4.0;
+		double c = Math.sqrt(a * a + b * b);
+		System.out.println("Hypotenuse is " + c);
+
+	}
+
+}
