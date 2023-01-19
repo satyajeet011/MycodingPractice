cbd5b4c31e18f1ad4a57a64fec0a6991ca40ed4c
SATYAJEET KUMAR SINGH <88815936+satyajeet011@users.noreply.github.com>
2023-01-19 23:09:37 +0530

    Add files via upload

---
+++ mycodingprac/src/WideNarrowDemo.java
@@ -0,0 +1,43 @@
+package abdulbari;
+
+import java.util.ArrayList;
+
+public class WideNarrowDemo {
+
+	public static void main(String[] args) {
+        byte b=5;
+        short s=120;
+        int i=10;
+        long l=1097687687L;
+        //float f=10;
+        float f=10.5f;
+        double d=10;
+        char c=10;
+        boolean bl=true;
+        
+        l=(int)s;
+        System.out.println(f);
+       /* i=s;
+        l=s;
+        f=s;
+        d=s;*/
+        
+        /*ArrayList<Integer> array1 = new ArrayList<>();
+        
+        
+        System.out.println(Integer.valueOf(i));
+        System.out.println(Integer.valueOf("65"));
+        
+        int p = s;
+        p = b;
+        p=i;
+        p=(int)l;
+        Integer val = (int) s;
+        
+        i=(int)f;
+        
+        Integer q = (int)f;
+        
+        System.out.println(i);*/
+    }
+}
