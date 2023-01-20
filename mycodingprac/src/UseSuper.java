ef5118ded05450b5c61b8abc6356283181615b21
SATYAJEET KUMAR SINGH <88815936+satyajeet011@users.noreply.github.com>
2023-01-20 23:10:34 +0530

    Add files via upload

---
+++ mycodingprac/src/UseSuper.java
@@ -0,0 +1,24 @@
+package Java;
+
+//Using super to overcome name hiding.
+class Abc {
+int i;
+}
+//Create a subclass by extending class A.
+class Babc extends Abc {
+int i; // this i hides the i in A
+Babc(int a, int b) {
+super.i = a; // i in A
+i = b; // i in B
+}
+void show() {
+System.out.println("i in superclass: " + super.i);
+System.out.println("i in subclass: " + i);
+}
+}
+class UseSuper {
+public static void main(String args[]) {
+Babc subOb = new Babc(1, 2);
+subOb.show();
+}
+}
