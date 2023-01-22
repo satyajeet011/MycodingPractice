92a702be44019ca0d9913b4a937b2745550c483a
SATYAJEET KUMAR SINGH <88815936+satyajeet011@users.noreply.github.com>
2023-01-22 23:32:01 +0530

    Add files via upload

---
+++ mycodingprac/src/EvenCount.java
@@ -0,0 +1,14 @@
+package Java2;
+import java.util.*;
+public class EvenCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("enter the number");
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+
+
+	}
+
+}
