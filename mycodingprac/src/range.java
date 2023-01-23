198a3671932da0f280c925459f086c733ce954fa
SATYAJEET KUMAR SINGH <88815936+satyajeet011@users.noreply.github.com>
2023-01-23 23:53:00 +0530

    Add files via upload

---
+++ mycodingprac/src/range.java
@@ -0,0 +1,20 @@
+package Java2;
+import java.util.*;
+
+public class range {
+	public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+       
+              int x =sc.nextInt();
+        if (x >=70 && x<=44000)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("No");
+        
+        }
+    }
+}
