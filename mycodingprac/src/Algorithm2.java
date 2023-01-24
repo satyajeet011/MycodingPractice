1c1902735d104639d475d695b3b2dd8984b3afa7
SATYAJEET KUMAR SINGH <88815936+satyajeet011@users.noreply.github.com>
2023-01-24 22:17:47 +0530

    Add files via upload

---
+++ mycodingprac/src/Algorithm2.java
@@ -0,0 +1,36 @@
+package TechMahindra;
+
+public class Algorithm2 {
+	public static void main(String[] args) {
+		 long startTime = System.currentTimeMillis();
+		 int x = 9999996;
+		 int n= 10000000;
+		 int count=0;
+		 boolean isEven = n%2 == 0;
+		 if(x>=0){
+		 if(isEven){
+		 for (int i = 0; i < n; i=i+2) {
+		 count++;
+		 if (i == x) {
+		 System.out.println("We found the values at "+count+" try");
+		 break;
+		 } 
+		 } 
+		 }else{
+		 for (int i = 1; i < n; i=i+2) {
+		 count++;
+		 if (i == x) {
+		 System.out.println("We found the values at "+count+" try");
+		 break;
+		 } 
+		 } 
+		 }
+		 }else{
+		 System.out.println("oops we are expecting vailid value");
+		 }
+		 
+		 long endTime = System.currentTimeMillis();
+		 System.out.println("Total time :: "+(endTime-startTime) + " ms");
+		 }
+
+}
