a31426599addbbc4d5dea50593ecde66bc517c41
SATYAJEET KUMAR SINGH <88815936+satyajeet011@users.noreply.github.com>
2023-01-24 22:16:52 +0530

    Add files via upload

---
+++ mycodingprac/src/Algorithm1.java
@@ -0,0 +1,20 @@
+package TechMahindra;
+
+public class Algorithm1 {
+	public static void main(String[] args) {
+		 long startTime = System.currentTimeMillis();
+		 int x = 9999996;
+		 int n = 10000000;
+		 int count = 0;
+		 for (int i = 0; i < n; i++) {
+		 count++;
+		 if (i == x) {
+		 System.out.println("We found the values at " + count + " try");
+		 break;
+		 }
+		 }
+		 long endTime = System.currentTimeMillis();
+		 System.out.println("Total time :: " + (endTime - startTime) + " ms");
+		 }
+
+}
