7d15781e42c28b074ba994329b19b9f0061b4aae
SATYAJEET KUMAR SINGH <88815936+satyajeet011@users.noreply.github.com>
2024-10-28 21:55:46 +0530

    Add files via upload

---
+++ mycodingprac/src/ArraySum.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class ArraySum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum =0;
+		int x[]= {100,200,300,400,500,600,700,800,900,1000};
+	      for (int i=0;i<=x.length-1;i++)
+	      {
+	    	  sum = sum+x[i];
+	{
+		System.out.println(sum);
+	}
+	      }
+	}
+
+}
